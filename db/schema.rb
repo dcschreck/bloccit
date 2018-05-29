b46e7901e8529f13bdc3c46e6bc3518793f3546f
Douglas Schreck <dcschreck421@gmail.com>
2018-05-29 17:06:04 +0300

    Checkpoint 27: issue with role assignment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_28_091221) do
+ActiveRecord::Schema.define(version: 2018_05_29_120221) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2018_05_28_091221) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
